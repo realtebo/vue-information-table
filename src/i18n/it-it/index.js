c1f430a09f9bacd7101b290e3fbfe7e1e83341aa
Realtebo <mirkotebaldi@yahoo.it>
2020-02-29 22:08:45 +0100

    Proseguimento traduzione italiana

--- src/i18n/it-it/index.js
+++ src/i18n/it-it/index.js
@@ -1,6 +1,8 @@
 export default {
   "Actually you are not member of any congregation":
     "Per ora non sei parte di nessuna congregazione",
+  "After a new user registers to this app, he/she must choose if create a new congregation, or join to an existing one":
+    "Quando un nuovo utente si registra in questa app, deve scegliere se creare una nuova congregazione o unirsi ad una già esistente",
   "Anagraphics and Roles": "Anagrafiche e Incarichi",
   "Associate with an existing one; get the invitation code and you will be able to view the pledges, but not administer them":
     "Associati ad una già esistente; fatti dare il codice di invito e potrai visualizzare gli impegni, ma non amministrarli",
@@ -21,11 +23,25 @@ export default {
   Dashboard: "Bacheca",
   "For Christian Life Meeting": "Per l'adunanza Vita Cristiana",
   "For Public Meetings": "Per l'adunanza pubblica",
+  "Generate the invitation code": "Genera il codice di invito",
   Existing: "Esistente",
   Home: "Pagina inziale",
+  "If you have already generated the invitation code, and you have communicated it to the user, do not generate a new code otherwise the user you sent will not be able to complete the registration":
+    "Se hai già generato il codice di invito, e lo hai comunicato all'utente, non generare un nuovo codice altrimenti l'utente che hai inviato non riuscira a completare la registrazione",
+  "If you haven't generated the code yet, click on the button below to generate a new code, then tell the user you want to invite":
+    "Se non hai ancora generato il codice, clicca sul pulsante qui sotto per generare un nuovo codice, quindi comunicalo all'utente che vuoi invitare",
+  "Instruct the user": "Spiega la procedura all'utente",
+  Istructions: "Istruzioni",
+  "Invitation code": "Codice d'invito",
+  "invite other to join this congregation":
+    "Invita altri ad unirsi a questa congregazione",
+  "It's your turn": "Ora tocca a te",
+  "You, as admin, can find the code, if already generated, in the Congregation Setting Page":
+    "Tu, come amministratore, puoi trovare il codice, se già generato, anche nella pagine delle impostazioni della congregazione",
   Login: "Accedi",
   Logout: "Esci",
   "meetings for field service": "Adunanze per il ministero di campo",
+  "members of your congregation": "Membri della tua congregazione",
   "microphone handlers": "microfonisti",
   "Mixer, Pc, etc...": "Mixer, Pc, etc...",
   "Name of the new congregation": "Nome della nuova congregazione",
@@ -34,10 +50,19 @@ export default {
   prayers: "preghiere",
   publishers: "proclamatori",
   schedules: "programmi",
+  "see istructions and generate invitation code":
+    "Leggi le istruzioni e genera il codice d'invito",
   Settings: "Impostazioni",
   "Sorry, nothing here": "Spiacente, ma qui non c'è nulla",
   "sound department": "reparto acustica",
+  "Tell him/her to choose to join an existing one":
+    "Spiegagli/le che dovrà scegliere di unirsi ad una congregazione già esistente",
+  "Then, him/her must insert the invitation code that you can find or generate below these instructions":
+    "A questo punto, dovrà inserire il codice di invito che puoi trovare, o generare, sotto a queste istruzioni",
+  "This feature is actually being develope, it's coming as soon as possible":
+    "Questa funzionalità è in corso di sviluppo, arriverà appena possibile",
   "View and Edit Schedules": "Visualizza e modifica i programmi",
   "watchtower reader": "lettore torre di guardia",
-  "watchtower's readers": "lettori torre di guardia"
+  "watchtower's readers": "lettori torre di guardia",
+  "What other user needs": "Di cosa han bisogno i nuovi utenti"
 };
